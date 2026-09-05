a8fb13bbe69bdc567b2e0f7212ce9f73a092aaa0
Livia Ami Webber <liviaami@outlook.com>
2026-09-05 14:23:39 +0200

    Updated code within Vehiclestate - abstract state interface

---
+++ state/VehicleState.cpp
@@ -0,0 +1,14 @@
+#include "VehicleState.h"
+#include "../vehicle/Vehicle.h"
+#include <iostream>
+
+void VehicleState::load(Vehicle& vehicle)   { reject(vehicle, "load"); }
+void VehicleState::depart(Vehicle& vehicle) { reject(vehicle, "depart"); }
+void VehicleState::arrive(Vehicle& vehicle) { reject(vehicle, "arrive"); }
+void VehicleState::delay(Vehicle& vehicle)  { reject(vehicle, "delay"); }
+void VehicleState::resume(Vehicle& vehicle) { reject(vehicle, "resume"); }
+ 
+void VehicleState::reject(Vehicle& vehicle, const std::string& action) const {
+    std::cout << "[Invalid Action] Vehicle " << vehicle.getId()
+              << " cannot '" << action << "' while " << getName() << ".\n";
+}
---
+++ state/VehicleState.h
@@ -0,0 +1,27 @@
+#ifndef VEHICLESTATE_H
+#define VEHICLESTATE_H
+ 
+#include <string>
+ 
+class Vehicle; // forward declaration - avoids circular include with Vehicle.h
+
+class VehicleState {
+public:
+    virtual ~VehicleState() = default;
+ 
+    virtual void load(Vehicle& vehicle);
+    virtual void depart(Vehicle& vehicle);
+    virtual void arrive(Vehicle& vehicle);
+    virtual void delay(Vehicle& vehicle);
+    virtual void resume(Vehicle& vehicle);
+ 
+    virtual std::string getName() const = 0;
+    virtual std::string describe() const = 0;
+
+    virtual bool isTemporary() const { return false; }
+ 
+protected:
+    void reject(Vehicle& vehicle, const std::string& action) const;
+};
+ 
+#endif
