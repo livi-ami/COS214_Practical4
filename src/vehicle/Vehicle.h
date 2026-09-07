8f3f2973cc6c4717663bb394d164ff2f53ea3ca9
Livia Ami Webber <liviaami@outlook.com>
2026-09-07 15:39:28 +0200

    Refactor Vehicle class and add state methods
    Refactor Vehicle class to improve state management and encapsulation.

--- src/vehicle/Vehicle.h
+++ src/vehicle/Vehicle.h
@@ -1,25 +1,33 @@
 #ifndef VEHICLE_H
 #define VEHICLE_H
+
 #include "../composite/TransportUnit.h"
+#include "../state/VehicleState.h"
 #include <string>
-using namespace std;
 
-class VehicleState;
+class Vehicle : public TransportUnit {
+public:
+    explicit Vehicle(const std::string& id);
+    ~Vehicle() override;
 
-class Vehicle: public TransportUnit{
-    protected:
-        Vehicle();
-    private:
-        VehicleState* state;
-        string id;
-    public:
-        Vehicle(string id);
-        ~Vehicle();
-        void addUnit(TransportUnit* unit);
-        VehicleState* getState();
-        void setState(VehicleState* state);
-        string getId();
-        void print() const;
+    // State delegation
+    void load();
+    void depart();
+    void arrive();
+    void delay();
+    void resume();
+
+    std::string getStatus() const;  //current state's short name
+    std::string report() const;     //longer state-dependent description
+
+    //called subclasses to switch the vehicle's current state
+    void setState(VehicleState* newState);
+
+    std::string getId() const override { return id; }
+
+private:
+    std::string id;
+    VehicleState* currentState;
 };
 
 #endif
